eab57a4a56c887301248be2f6e8dad2c0682dba1
CodingFactoryT <tim.baesler@gmail.com>
2023-02-20 20:06:58 +0100

    added AppIcon to Window

--- src/main/java/org/CodingFactoryT/PDFRotator/MainFrame.java
+++ src/main/java/org/CodingFactoryT/PDFRotator/MainFrame.java
@@ -1,9 +1,12 @@
 package org.CodingFactoryT.PDFRotator;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.dnd.DropTarget;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class MainFrame extends JFrame {
     public MainFrame() throws IOException {
@@ -20,8 +23,8 @@ public class MainFrame extends JFrame {
         } catch (UnsupportedLookAndFeelException e) {
             throw new RuntimeException(e);
         }
-
         this.setTitle("PDF Rotator");
+        this.setIconImage(ImageIO.read(getClass().getResourceAsStream("/AppIcon.png")));
         this.setPreferredSize(new Dimension(1280, 720));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
